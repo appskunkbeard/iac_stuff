8a19ffb2002620e4d2464668be70e2a145db4214
eugene <eugene@oldgoat.localhost>
2023-05-29 10:39:16 -0500

    updated main.tf to create a web server

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ resource "aws_instance" "example" {
   ami		= "ami-01107263728f3bef4"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
+
   tags = {
    Name = "terraform-example"
   }
