e286d1657b39937e4691419c25bbe377ceb2fca4
eugene <eugene@oldgoat.localhost>
2023-06-04 12:00:08 -0500

    Added varibles

--- main.tf
+++ main.tf
@@ -5,14 +5,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description   = "The port number used for this test HTTP server port number"
+  type          = number
+  default       = 8080
+}
+
 resource "aws_instance" "example" {
-  ami		= "ami-01107263728f3bef4"
-  instance_type = "t2.micro"
+  ami		         = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id] 
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
 
@@ -21,3 +28,20 @@ resource "aws_instance" "example" {
    Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port    = var.server_port
+    to_port      = var.server_port
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"] 
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP addr of the web server"
+  }
+
